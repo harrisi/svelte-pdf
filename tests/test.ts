c09ec6be06472af36334e837fcdc7402dea923f6
Ian Harris <ian@fofgof.xyz>
2023-09-27 14:08:14 -0700

    fix test

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,9 @@ test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
 });
+
+test('gen pdf', async ({ page }) => {
+  await page.goto('/')
+  await page.pdf({ path: 'foo.pdf' })
+  await expect(true).toBeTruthy()
+})
